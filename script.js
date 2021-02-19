494a3033bfce91ede63edfbc7b8d87b4403ec378
diarm0d <diarmuid.odonovan@protonmail.com>
2021-02-19 14:52:38 +0000

    added user journey & copy email functionality

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const portfolioSections = document.querySelector('section');
+const portfolioHome =document.getElementById('portfolio-home');
+const portfolioSections = document.querySelectorAll('.portfolio-projects');
+const emailInput = document.getElementById('email');
 const splashImage = document.getElementById('image-wrapper');
 const portfolioBtn = document.getElementById('explore-portfolio');
+const contactBtn = document.getElementById('contact-button');
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const toggleIcon = document.getElementById('toggle-icon');
+const backupBtn = document.getElementById('back-up-button');
+
 
 function toggleDarkLightMode(isDark) {
     isDark ? toggleIcon.children[0].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[0].classList.replace('fa-moon', 'fa-sun');
     isDark ? splashImage.classList.add('dark') :  splashImage.classList.remove('dark');
 }
 
-console.log(splashImage.style.filter);
-
-// 
 // Swtich Theme
 function switchTheme(event) {
     if (event.target.checked) {
@@ -26,13 +28,45 @@ function switchTheme(event) {
 }
 
 function viewPortfolio() {
-    portfolioSections.style.display = "block";
+    for (i = 0; i < portfolioSections.length; i++) {
+        var projectSelect = portfolioSections[i];
+        projectSelect.classList.remove('portfolio-display');
+    }
     window.location.hash = "portfolio-project-1";
+    portfolioHome.classList.add('portfolio-display');
 }
+
+function exitPortfolio() {
+    portfolioHome.classList.remove('portfolio-display');
+    window.location.hash = "portfolio-home";
+    for (i = 0; i < portfolioSections.length; i++) {
+        var projectSelect = portfolioSections[i];
+        projectSelect.classList.add('portfolio-display');   
+    }
+}
+
+
+// Copy email address
+function copy() {
+    var copyText = emailInput.value;
+    copyToClipboard(copyText);
+    contactBtn.title = 'Copied';
+  }
+
+  const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
 portfolioBtn.addEventListener('click', viewPortfolio);
+backupBtn.addEventListener('click', exitPortfolio);
+contactBtn.addEventListener('click', copy);
 
 // Check local Storage for Theme
 const currentTheme = localStorage.getItem('theme');
